77f85ecb83947d118f7be8d62baebb4dac4f4914
Mike Lau <princekalpakjian@gmail.com>
2026-05-25 09:51:09 +0800

    Refactor EnemyWeapon class to enhance damage handling and improve code structure

--- Assets/Script/Enemy/EnemyWeapon.cs
+++ Assets/Script/Enemy/EnemyWeapon.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
 
-public class EnemyWeapon : MonoBehaviour {
+public class EnemyWeapon : MonoBehaviour
+{
+    [HideInInspector]
+    public float attackDamage;
+    public float weaponDamage;
 
-	[HideInInspector]
-	public float attackDamage;
-	public float weaponDamage;
+    [HideInInspector]
+    public AttackType type = AttackType.normal;
 
-	void OnTriggerEnter (Collider col) {
-		if (col.CompareTag("Player") && attackDamage > 0)
-			col.SendMessage("Hurt", attackDamage + weaponDamage);
-	}
+    [HideInInspector]
+    public int strength = 1;
+
+    Enemy owner;
+
+    void Start()
+    {
+        owner = GetComponentInParent<Enemy>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!col.CompareTag("Player") || attackDamage <= 0)
+            return;
+
+        PlayerController player = col.GetComponent<PlayerController>();
+        if (!player)
+            player = col.GetComponentInParent<PlayerController>();
+
+        if (player)
+        {
+            AttackData attack = new AttackData();
+            attack.attacker = owner ? owner.gameObject : gameObject;
+            attack.attackerFaction = Faction.Enemy;
+            attack.damage = attackDamage + weaponDamage;
+            attack.position = transform.position;
+            attack.type = type;
+            attack.strength = strength;
+
+            player.TakeDamage(attack);
+        }
+    }
 }
-
-
